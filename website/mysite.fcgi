f9006098b0f8d163d20463fdd4cb9c778a577e24
Eric Price <ecprice@mit.edu>
2012-06-17 22:41:12 -0400

    deal with website move

--- website/mysite.fcgi
+++ website/mysite.fcgi
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 import sys, os
 
-DIR='/mit/ecprice/web_scripts'
+DIR='/mit/ecprice/web_scripts/newsdiffs'
 
 # Add a custom Python path.
 sys.path.insert(0, DIR)
-sys.path.insert(0, DIR+"/newsdiffer")
+sys.path.insert(0, DIR+"/website")
 
 # Switch to the directory of your project. (Optional.)
 # os.chdir("/home/user/myproject")
 
 # Set the DJANGO_SETTINGS_MODULE environment variable.
-os.environ['DJANGO_SETTINGS_MODULE'] = "newsdiffer.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "website.settings"
 
 from django.core.servers.fastcgi import runfastcgi
 runfastcgi(method="threaded", daemonize="false")
--- website/settings.py
+++ website/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'newsdiffer.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '../newsdiffer.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'ecprice'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'xxx'         # Not used with sqlite3.
 DATABASE_HOST = 'sql.mit.edu'             # Set to empty string for localhost. Not used with sqlite3.
@@ -63,7 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'newsdiffer.urls'
+ROOT_URLCONF = 'website.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
