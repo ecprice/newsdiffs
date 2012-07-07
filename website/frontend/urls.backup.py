9f989f0bb4e7ca6a471ec88101ec425e4033b0e3
Jennifer 8. Lee <jenny@jennifer8lee.com>
2012-07-07 16:00:18 -0400

    changing the 404.html to a template

--- website/frontend/urls.backup.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-  #
-  (r'^diffview', 'frontend.views.diffview'),
-  (r'^view', 'frontend.views.view'),
-  (r'^', 'frontend.views.index'),
-)
