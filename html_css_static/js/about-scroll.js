c4a022e9190de90bdc45dd842312c05e67b7548c
paulahaertel <paulahaertel@yahoo.de>
2015-06-08 13:07:16 +0200

    changed bottom margin of searchbar

---
+++ html_css_static/js/about-scroll.js
@@ -0,0 +1,16 @@
+// Source https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
