7c9af9bf7bf0b158ce818400ef0cdd6f62dbd0e4
Eric Price <ecprice@mit.edu>
2015-10-17 23:20:04 -0700

    Parse WaPo blogs

--- parsers/washpo.py
+++ parsers/washpo.py
@@ -2,6 +2,7 @@ from baseparser import BaseParser
 from bs4 import BeautifulSoup
 import re
 import datetime
+import dateutil.parser
 
 DATE_FORMAT = '%A, %B %e %Y, %l:%M %p'
 
@@ -9,11 +10,38 @@ class WashPoParser(BaseParser):
     SUFFIX = '?print=true'
     domains = ['www.washingtonpost.com']
 
-    feeder_pat   = '^http://www.washingtonpost.com/.*_story.html'
+    feeder_pat   = '^https?://www.washingtonpost.com/.*_story.html|https?://www.washingtonpost.com/.*/wp/.*/'
     feeder_pages = ['http://www.washingtonpost.com/']
 
     def _printableurl(self):
         return re.sub('_story.html.*', '_print.html', self.url)
+
+    def _blog_parse(self, soup):
+        elt = soup.find('h1', itemprop="headline")
+        if elt is None:
+            return False
+        self.title = elt.getText().strip()
+
+        elt = soup.find('span', itemprop='author')
+        if elt is None:
+            self.byline = ''
+        else:
+            self.byline = elt.getText().strip()
+
+        elt = soup.find('span', itemprop="datePublished")
+        if elt is None:
+            self.date = ''
+        else:
+            datestr = elt['content']
+            if datestr[-2:] == u'00' and datestr[-3] != u':':
+                datestr = datestr[:-2] + u':00' # fix timezone formatting
+            date = dateutil.parser.parse(datestr)
+            self.date = date.strftime(DATE_FORMAT)
+        div = soup.find('article', itemprop='articleBody')
+        if div is None:
+            return False
+        self.body = '\n'+'\n\n'.join([x.getText().strip() for x in div.findAll('p')])
+        return True
 
     def _parse(self, html):
         soup = BeautifulSoup(html)
@@ -21,7 +49,8 @@ class WashPoParser(BaseParser):
         self.meta = soup.findAll('meta')
         elt = soup.find('h1', property="dc.title")
         if elt is None:
-            self.real_article = False
+            if not self._blog_parse(soup):
+                self.real_article = False
             return
         self.title = elt.getText().strip()
         elt = soup.find('h3', property="dc.creator")
